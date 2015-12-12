444ca2724d27cd4f08ea1a747bd18f1ff8bf299d
AdrianoTosetto <adrianogostosoelindo@gmail.com>
2015-12-12 19:33:03 -0200

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
-
+var canvas;
+var ctx;
 var player = {
 	
 	x:0,
 	y:0,
 	height:20,
-	width:80
+	width:80,
+	move:function(key){
+		if(key == 37){
+			this.x++;
+			console.log(this.x);
+		}	
+	}
 };
 
 var draw = function(){
-	var canvas = document.getElementById("game");
-	var ctx    = canvas.getContext("2d");
-	player.x = (canvas.width - player.width)/2;
-	player.y = canvas.height - player.height;
 	ctx.fillRect(player.x,player.y,player.width,player.height);
 }
 
+var update = function(){
+	document.onkeydown = function(key){
+		player.move(key);
+	}
+}
+
 window.onload = function(){
-	draw();
+
+	canvas =  document.getElementById("game");
+	ctx    = canvas.getContext("2d");
+	player.x = (canvas.width - player.width)/2;
+	player.y = canvas.height - player.height;
+	window.setInterval(function(){ //it will be used resquestAnimationFrame
+		update();
+		draw();
+	},500);
 }
