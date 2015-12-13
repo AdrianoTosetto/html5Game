71ad144c21cbabdd41f9cec2f6dab746dd3f5372
AdrianoTosetto <adrianogostosoelindo@gmail.com>
2015-12-12 22:12:57 -0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 var canvas;
 var ctx;
+var Bullet =  function(x,y){
+	this.x = x;
+	this.y = y;
+	this.height = 20;
+	this.width  = 20;
+};
 var player = {
-	
+	bullets:[new Bullet(0,0)],
 	x:0,
 	y:0,
 	height:20,
@@ -9,24 +15,34 @@ var player = {
 	move:function(key){
 		if(key.keyCode == 37){
 			this.x-=3;
-			console.log(this.x);
 		}
 		if(key.keyCode == 39){
 			this.x+=3;
-			console.log(this.x);
 		}
-
+	},
+	shoot:function(key){
+		if(key.keyCode == 32){
+			this.bullets.push(new Bullet(player.x,player.y - player.height - 1));
+			console.log(player.bullets.length)
+		}
 	}
 };
 
 var draw = function(){
 	ctx.clearRect(0,0,canvas.height,canvas.width);
 	ctx.fillRect(player.x,player.y,player.width,player.height);
+	for(var i = 0; i < player.bullets.length;i++){
+		ctx.fillRect(player.bullets[i].x,player.bullets[i].y,20,20);	
+	}
+	
+	console.log(player.bullets[0].x);
 }
 
 var update = function(){
 	document.onkeydown = function(key){
 		player.move(key);
+		player.shoot(key);
+		
 	}
 }
 
@@ -39,5 +55,5 @@ window.onload = function(){
 	window.setInterval(function(){ //it will be used resquestAnimationFrame
 		update();
 		draw();
-	},100);
+	},50);
 }
